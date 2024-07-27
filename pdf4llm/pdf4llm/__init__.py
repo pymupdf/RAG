7b4677184ec983ce627a55e3284632b4dcac2652
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2024-07-28 01:44:07 +0300

    chore: delete `pdf4llm` package

--- pdf4llm/pdf4llm/__init__.py
+++
@@ -1,7 +0,0 @@
-__version__ = "0.1.0"
-version = __version__
-version_tuple = tuple(map(int, version.split(".")))
-
-raise RuntimeError(
-    "The pdf4llm package is now deprecated. Please use pymupdf4llm instead."
-)
