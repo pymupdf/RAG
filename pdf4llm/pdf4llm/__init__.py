d708fbdc8920b82570867c2fdf9e1dd6455f9ad1
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2024-07-28 01:41:52 +0300

    chore: deprecate `pdf4llm` package

--- pdf4llm/pdf4llm/__init__.py
+++ pdf4llm/pdf4llm/__init__.py
@@ -1,13 +1,7 @@
-import pymupdf4llm
-from pymupdf4llm import *
-
-
-__version__ = "0.0.9"
+__version__ = "0.1.0"
 version = __version__
 version_tuple = tuple(map(int, version.split(".")))
 
-
-def LlamaMarkdownReader(*args, **kwargs):
-    from pymupdf4llm.llama import pdf_markdown_reader
-
-    return pymupdf4llm.llama.pdf_markdown_reader.PDFMarkdownReader(*args, **kwargs)
+raise RuntimeError(
+    "The pdf4llm package is now deprecated. Please use pymupdf4llm instead."
+)
